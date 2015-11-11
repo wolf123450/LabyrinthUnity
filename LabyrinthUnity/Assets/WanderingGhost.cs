fe9b080b7840a83fa0d7d3598968e2af5273e07d
Drew Harker <Home@Computer-Name.local>
2015-11-11 14:17:08 -0700

    Added ghost character, and started GhostAI

---
+++ LabyrinthUnity/Assets/WanderingGhost.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WanderingGhost : MonoBehaviour {
+	Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponentInParent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//add random rotation to body
+
+		//if(not moving too fast)
+		body.AddForce (transform.forward * 0.2f);
+	}
+}
