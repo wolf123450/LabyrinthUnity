b1b768ca8df8cef68b40915c9529c7466c229966
Drew Harker <Home@Computer-Name.local>
2015-11-23 10:43:42 -0700

    Added tags for the Player and enemies

--- LabyrinthUnity/Assets/WanderingGhost.cs
+++ LabyrinthUnity/Assets/WanderingGhost.cs
@@ -27,7 +27,7 @@ public class WanderingGhost : MonoBehaviour {
 	}
 	
 	void OnTriggerEnter(Collider obj) {
-		if (obj.name.Equals ("Player")) {
+		if (obj.tag.Equals ("Player")) {
 			alert = true;
 			turning = false;
 			target = obj.attachedRigidbody;
